b13a7a41fc3f7593b69cb5d79120b970973296ea
SALMON-PC\hiroaki <salmon2073@gmail.com>
2018-05-14 20:08:43 +0900

    閾値の修正

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const map = (value, fromMin, fromMax, toMin, toMax) => {
     return result;
 };
 
-app.get("/:lat/:lng", (req, res) => {
+app.get("/:lng/:lat", (req, res) => {
     console.log(req.params);
     let lng = req.params.lng
     let lat = req.params.lat
@@ -25,24 +25,24 @@ app.get("/:lat/:lng", (req, res) => {
     console.log(lng, lat)
 
     let i = Math.floor(map(lng, 100, 170, 0, 64))
-    let j = Math.floor(map(lat, 7, 61, 0, 64))
+    let j = Math.floor(map(lat, 7, 61, 64, 0))
 
     let x = Math.floor(map(lng, 100, 170, 0, mapLength) - (i * tileLength));
-    let y = Math.floor(map(lat, 7, 61, 0, mapLength) - (j * tileLength));
+    let y = Math.floor(map(lat, 7, 61, mapLength, 0) - (j * tileLength));
 
-    let url = "https://www.jma.go.jp/jp/highresorad/highresorad_tile/HRKSNC/201805140815/201805140815/zoom6/" + i + "_" + j + ".png";
+    let url = "https://www.jma.go.jp/jp/highresorad/highresorad_tile/HRKSNC/201805141050/201805141050/zoom6/" + i + "_" + j + ".png";
 
     Jimp.read(url)
         .then(image => {
             let hexColor = image.getPixelColor(x, y)
             let rgbColor = Jimp.intToRGBA(hexColor);
-            let result = {};
+            let result = {'min':"null"};
 
             if (rgbColor.r == 0 && rgbColor.g == 0 && rgbColor.b == 0) {
                 result.min = 0
                 result.max = 0
             }
-            if (rgbColor.r == 255 && rgbColor.g == 255 && rgbColor.b == 255) {
+            if (rgbColor.r == 242 && rgbColor.g == 242 && rgbColor.b == 255) {
                 result.min = 0
                 result.max = 1
             }
@@ -50,11 +50,11 @@ app.get("/:lat/:lng", (req, res) => {
                 result.min = 1
                 result.max = 5
             }
-            if (rgbColor.r == 27 && rgbColor.g == 140 && rgbColor.b == 255) {
+            if (rgbColor.r == 33 && rgbColor.g == 140 && rgbColor.b == 255) {
                 result.min = 5
                 result.max = 10
             }
-            if (rgbColor.r == 0 && rgbColor.g == 53 && rgbColor.b == 255) {
+            if (rgbColor.r == 0 && rgbColor.g == 65 && rgbColor.b == 255) {
                 result.min = 10
                 result.max = 20
             }
@@ -76,8 +76,11 @@ app.get("/:lat/:lng", (req, res) => {
             }
 
             console.log(rgbColor)
-            console.log(i, j)
-            res.send(result)
+            console.log("i:" + i, "j:" + j)
+            console.log("x:" + x, "y" + y)
+            console.log("result: " + result.min, result.max)            
+            res.send("<img src='" + url + "'><p></p>")
+            // res.send(result)
         });
 })
 
